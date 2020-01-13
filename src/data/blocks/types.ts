99d838e4451ec7eadf17c4136197bfa12073ecfb
Ruszard <nekorushi@gmail.com>
2020-01-13 03:26:15 +0100

    Fix block assets paths

--- src/data/blocks/types.ts
+++ src/data/blocks/types.ts
@@ -13,12 +13,12 @@ export enum BlockColor {
 export type BlockVariant = BlockColor[][]
 
 export const BlockImages = {
-    [BlockColor.Blue]: 'block_blue.png',
-    [BlockColor.Green]: 'block_green.png',
-    [BlockColor.LightBlue]: 'block_light_blue.png',
-    [BlockColor.Orange]: 'block_orange.png',
-    [BlockColor.Pink]: 'block_pink.png',
-    [BlockColor.Red]: 'block_red.png',
-    [BlockColor.Violet]: 'block_violet.png',
-    [BlockColor.Yellow]: 'block_yellow.png'
+    [BlockColor.Blue]: 'assets/images/blocks/block_blue.png',
+    [BlockColor.Green]: 'assets/images/blocks/block_green.png',
+    [BlockColor.LightBlue]: 'assets/images/blocks/block_light_blue.png',
+    [BlockColor.Orange]: 'assets/images/blocks/block_orange.png',
+    [BlockColor.Pink]: 'assets/images/blocks/block_pink.png',
+    [BlockColor.Red]: 'assets/images/blocks/block_red.png',
+    [BlockColor.Violet]: 'assets/images/blocks/block_violet.png',
+    [BlockColor.Yellow]: 'assets/images/blocks/block_yellow.png'
 }
